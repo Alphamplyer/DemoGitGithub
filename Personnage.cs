5ed1c94f3b31860c90526561005c78724828cee7
Alphamplyer <even@pouyadou.fr>
2018-08-07 17:12:39 +0200

    Ajout de la caractéristique Armure sur Personnage + son constructeur

--- Personnage.cs
+++ Personnage.cs
@@ -5,12 +5,23 @@ public class Personnage {
 
     private int hp;
     private int damage;
+    private int armor;
     private float moveSpeed;
 
     public int HP { get => hp ; set => hp = value; }
+    public int Armor { get => armor ; set => armor = value; }
+
+    public Personnage(int hp, int damage, int armor, int moveSpeed)
+    {
+        this.hp = hp;
+        this.damage = damage;
+        this.armor = armor;
+        this.moveSpeed = moveSpeed;
+    }
 
     public void Attack(Personnage personnage)
     {
-        personnage.HP -= damage;
+        if (armor < damage)
+            personnage.HP -= damage - armor;
     }
 }
